a1ad58f0a10e11e19ebfe3dc7c90f548d082eaa2
Daniel Sloof <daniel@rubic.nl>
2022-06-03 19:38:19 +0200

    allow setting min viewers

--- src/FindCommand.php
+++ src/FindCommand.php
@@ -7,12 +7,14 @@ namespace TwitchDiscordFinder;
 use TwitchDiscordFinder\Twitch\StreamFinder;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class FindCommand extends Command
 {
     private const ARGUMENT_GAME = 'game';
+    private const OPTION_MIN_VIEWERS = 'min-viewers';
 
     public function __construct(private StreamFinder $streamFinder, string $name = null)
     {
@@ -23,15 +25,22 @@ class FindCommand extends Command
     {
         $this
             ->addArgument(self::ARGUMENT_GAME, InputArgument::REQUIRED)
+            ->addOption(self::OPTION_MIN_VIEWERS, null, InputOption::VALUE_OPTIONAL, 'Minimum number of viewers', 10)
             ->setName('tdf:find');
     }
     
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        echo implode(PHP_EOL, array_map(function($stream) {
-            return $stream->getStreamer();
-        }, $this->streamFinder->find([], $input->getArgument(self::ARGUMENT_GAME))));
-
+        echo implode(
+            PHP_EOL,
+            array_map(function($stream) {
+                return $stream->getStreamer() . ' ' . $stream->getViewers();
+            }, $this->streamFinder->find(
+                [],
+                $input->getArgument(self::ARGUMENT_GAME),
+                (int) $input->getOption(self::OPTION_MIN_VIEWERS)
+            ))
+        );
         return 0;
     }
 }
